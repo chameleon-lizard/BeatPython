1cedc77c4794826110bc91531b61f1e48a55d916
Arina Okshus <me@chameleon-lizard.ru>
2022-09-10 16:37:08 +0300

    Refactoring.

--- game.py
+++ game.py
@@ -3,46 +3,66 @@ from typing import Tuple
 import pygame as pg
 import tracking
 
-def landmark_to_game(landmark, imsize) -> Tuple[int, int, int]:
-    # According to mediapipe's documentation, the coordinates are normalized
-    # to a range of [0, 1] relative to the image size.
-    return (
-                int(landmark.x * imsize[0]), 
-                int(landmark.y * imsize[1]), 
-                int(landmark.z * imsize[0])
+class Game:
+    def __init__(self):
+        # Initializing pygame
+        pg.init()
+        self._imsize = (500, 500)
+        self._screen = pg.display.set_mode(self._imsize)
+
+        # Creating an object for tracking the pose
+        self._landmarks = tracking.Track(0, self._imsize)
+
+        # Defining the colors
+        self._background_color = (255, 255, 255)
+        self._lhand_color = (0, 0, 255)
+        self._rhand_color = (255, 0, 0)
+
+    def run(self):
+        running = True
+        while running:
+            # Checking if the game is not quit by the user
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+            
+            # Acquiring keypoints from the camera stream
+            lhand, rhand = self.__get_point_data()
+
+            # Draw background
+            self._screen.fill(self._background_color)
+        
+            # Right and left hands
+            pg.draw.line(self._screen, self._rhand_color, (rhand[0][0], rhand[0][1]), (rhand[1][0], rhand[1][1]))
+            pg.draw.line(self._screen, self._lhand_color, (lhand[0][0], lhand[0][1]), (lhand[1][0], lhand[1][1]))
+
+            # Flip the display
+            pg.display.flip()
+
+    def __landmark_to_game(self, landmark) -> Tuple[int, int, int]:
+        # According to mediapipe's documentation, the coordinates are normalized
+        # to a range of [0, 1] relative to the image size.
+        return (
+                    int(landmark.x * self._imsize[0]), 
+                    int(landmark.y * self._imsize[1]), 
+                    int(landmark.z * self._imsize[0])
             )
 
+    def __get_point_data(self):
+        pose = self._landmarks.get_keypoints().pose_landmarks.landmark
+        
+        return (
+                    self.__landmark_to_game(pose[14]), 
+                    self.__landmark_to_game(pose[16])
+                ), \
+                (
+                    self.__landmark_to_game(pose[13]), 
+                    self.__landmark_to_game(pose[15])
+                )
+
+    def __del__(self):
+        pg.quit()
+
 if __name__ == "__main__":
-    pg.init()
-    imsize = (500, 500)
-    screen = pg.display.set_mode(imsize)
-    # Run until the user asks to quit
-    running = True
-    rhand_color = (255, 0, 0)
-    lhand_color = (0, 0, 255)
-    landmarks = tracking.Track(0, imsize)
-    while running:
-        # Did the user click the window close button?
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-
-        # Fill the background with white
-        screen.fill((255, 255, 255))
-
-        pose = landmarks.get_keypoints().pose_landmarks.landmark
-
-        rhand_start = landmark_to_game(pose[14], imsize)
-        rhand_end = landmark_to_game(pose[16], imsize)
-
-        lhand_start = landmark_to_game(pose[13], imsize)
-        lhand_end = landmark_to_game(pose[15], imsize)
-
-        pg.draw.line(screen, rhand_color, (rhand_start[0], rhand_start[1]), (rhand_end[0], rhand_end[1]))
-        pg.draw.line(screen, lhand_color, (lhand_start[0], lhand_start[1]), (lhand_end[0], lhand_end[1]))
-
-        # Flip the display
-        pg.display.flip()
-
-    # Done! Time to quit.
-    pg.quit()
+    game = Game()
+    game.run()
