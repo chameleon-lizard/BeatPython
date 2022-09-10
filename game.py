2ec19ea47855152ec372725f26f36469f758cb12
Arina Okshus <me@chameleon-lizard.ru>
2022-09-10 15:29:41 +0300

    Created basis for the application.

--- game.py
+++ game.py
@@ -1,14 +1,26 @@
+from typing import Tuple
+
 import pygame as pg
 import tracking
 
+def landmark_to_game(landmark, imsize) -> Tuple[int, int, int]:
+    # According to mediapipe's documentation, the coordinates are normalized
+    # to a range of [0, 1] relative to the image size.
+    return (
+                int(landmark.x * imsize[0]), 
+                int(landmark.y * imsize[1]), 
+                int(landmark.z * imsize[0])
+            )
+
 if __name__ == "__main__":
     pg.init()
-
-    screen = pg.display.set_mode([500, 500])
+    imsize = (500, 500)
+    screen = pg.display.set_mode(imsize)
     # Run until the user asks to quit
     running = True
-    line_color = (255, 0, 0)
-    landmarks = tracking.Track(0)
+    rhand_color = (255, 0, 0)
+    lhand_color = (0, 0, 255)
+    landmarks = tracking.Track(0, imsize)
     while running:
         # Did the user click the window close button?
         for event in pg.event.get():
@@ -18,12 +30,17 @@ if __name__ == "__main__":
         # Fill the background with white
         screen.fill((255, 255, 255))
 
-        line_start = landmarks[14]
-        line_end = landmarks[16]
-        print(line_start)
+        pose = landmarks.get_keypoints().pose_landmarks.landmark
 
-        pg.draw.line(screen, line_color, line_start, line_end)
-    
+        rhand_start = landmark_to_game(pose[14], imsize)
+        rhand_end = landmark_to_game(pose[16], imsize)
+
+        lhand_start = landmark_to_game(pose[13], imsize)
+        lhand_end = landmark_to_game(pose[15], imsize)
+
+        pg.draw.line(screen, rhand_color, (rhand_start[0], rhand_start[1]), (rhand_end[0], rhand_end[1]))
+        pg.draw.line(screen, lhand_color, (lhand_start[0], lhand_start[1]), (lhand_end[0], lhand_end[1]))
+
         # Flip the display
         pg.display.flip()
 
--- tracking.py
+++ tracking.py
@@ -1,20 +1,23 @@
-from typing import List
+from typing import Tuple
 
 import mediapipe as mp
 import numpy as np
 import cv2 as cv
 
 class Track:
-    def __init__(self, sink: int) -> None:
+    def __init__(self, sink: int, imsize: Tuple[int, int]) -> None:
+        self._imsize = imsize
         self._cap = cv.VideoCapture(sink)
         self._pose = mp.solutions.pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
     def __get_frame(self) -> np.array:
-        ret, frame = self._cap.read()
+        _, frame = self._cap.read()
+        frame = cv.resize(frame, self._imsize)
+        frame = cv.flip(frame, 1)
         return cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
-    def get_keypoints(self) -> List:
-        return self._pose.process(self.__get_frame()).pose_landmarks
+    def get_keypoints(self):
+        return self._pose.process(self.__get_frame())
 
     def __del__(self) -> None:
         self._cap.release()
